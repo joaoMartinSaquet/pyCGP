c1c7039a464d908eca668858769ec75c5a277193
MOI <jmartinsaquet@lrtechnologies.fr>
2025-06-12 14:09:05 +0200

    add pycgp submodule

--- pycgp/evaluators/__init__.py
+++ pycgp/evaluators/__init__.py
@@ -1,4 +1,5 @@
 from .srevaluator import SREvaluator
+from .evaluator import Evaluator
 # from .maskevaluator import MaskEvaluator
 
-__all__ = ['SrEvaluator', 'MaskEvaluator']
+__all__ = ['SrEvaluator', 'MaskEvaluator', 'Evaluator']
